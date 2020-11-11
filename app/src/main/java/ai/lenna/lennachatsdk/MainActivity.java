76d0262c631904d3d2dc48ef543c93cffdcf9b56
viky_lenna <viky@lenna.ai>
2020-11-11 14:03:20 +0700

    api 19

--- app/src/main/java/ai/lenna/lennachatsdk/MainActivity.java
+++ app/src/main/java/ai/lenna/lennachatsdk/MainActivity.java
@@ -3,10 +3,6 @@ package ai.lenna.lennachatsdk;
 import androidx.annotation.Keep;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import org.json.JSONObject;
-import ai.lenna.lennachatmodul.Chat;
-import ai.lenna.lennachatmodul.util.AesCipher;
 
 @Keep
 public class MainActivity extends AppCompatActivity {
@@ -15,17 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 //        setContentView(R.layout.activity_main);
-
-        Chat.setAppId("NBVO0y");
-        Chat.setBotId("PdRgRe");
-        Chat.setAppKey("gm+sCaMg5ai0vPes+tB83O3G0vaS4ahwGV+81Hnzr6jCbi7g+vYbmzHHcy/vH64jHMwq9pLr8z/eWXfVWZ4gPv64p6PvmW4aHWbnIfpF9SeKSRJGy+pXyMbiqBdzpOEurDhsLixpHvA21sUqlHPq71XJxLoNg9hPhWSfCexpzCh36OlnW1hpoX7YSNGVDRUtorCBcPerj/43UQVfeKCA+Q==");
-        Chat.setUserName("Pengunjung");
-        Chat.setIcon(R.drawable.logo_chat);
-        Chat.setIconBubleChat(R.drawable.logo_ancol);
-        Chat.setGreetingMessage("hai");
-        Chat.setKeyFallBack("locna");
-        Chat.setRequestMenuFAllback("fallback-locna");
-        Chat.start(this);
 
     }
 }
