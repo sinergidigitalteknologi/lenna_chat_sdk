35617b968b906f1c4e8e7234a1c7126fc02a62dc
Muhammad Arief Sosiawan <arief@lenna.ai>
2020-12-07 00:01:21 +0700

    New Update

--- app/src/main/java/com/example/testfirebase/MainActivity.java
+++ app/src/main/java/com/example/testfirebase/MainActivity.java
@@ -31,41 +31,6 @@ public class MainActivity extends AppCompatActivity {
 
         onHandler(this);
 
-//        FirebaseInstanceId.getInstance().getInstanceId()
-//                .addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
-//                    @Override
-//                    public void onComplete(@NonNull Task<InstanceIdResult> task) {
-//                        if (task.isSuccessful()) {
-//                            String token = task.getResult().getToken();
-//                            String msg = getString(R.string.msg_token_fmt, token);
-//
-//                            fcmToken = msg;
-//
-//                            Chat.setAppId("NBVO0y");
-//                            Chat.setBotId("PdRgRe");
-//                            Chat.setAppKey("gm+sCaMg5ai0vPes+tB83O3G0vaS4ahwGV+81Hnzr6jCbi7g+vYbmzHHcy/vH64jHMwq9pLr8z/eWXfVWZ4gPv64p6PvmW4aHWbnIfpF9SeKSRJGy+pXyMbiqBdzpOEurDhsLixpHvA21sUqlHPq71XJxLoNg9hPhWSfCexpzCh36OlnW1hpoX7YSNGVDRUtorCBcPerj/43UQVfeKCA+Q==");
-//                            Chat.setUserName("Pengunjungt");
-//                            Chat.setIcon(R.drawable.logo_ancol);
-//                            Chat.setIconBubleChat(R.drawable.logo_ancol);
-//                            Chat.setGreetingMessage("hai");
-//                            Chat.setKeyFallBack("locna");
-//                            Chat.setRequestMenuFAllback("fallback-locna");
-//                            Chat.setTokenFcm(fcmToken);
-//                            Chat.start(MainActivity.this);
-////                            Log.d("hai_token", token);
-////                            if (!fcmToken.equals("")) {
-//////                                handler.postDelayed(r, 0);
-////                            } else {
-////                                handler.removeCallbacks(r);
-////                            }
-//                        } else {
-////                            handler.removeCallbacks(r);
-//                        }
-//                    }
-//                });
-
-
-
         FirebaseInstanceId.getInstance().getInstanceId()
                 .addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
                     @Override
@@ -81,11 +46,12 @@ public class MainActivity extends AppCompatActivity {
                         String msg = getString(R.string.msg_token_fmt, token);
 
                         fcmToken = msg;
-                        if (!fcmToken.equals("")) {
-                            handler.postDelayed(r, 0);
-                        } else {
-                            handler.removeCallbacks(r);
-                        }
+                        handler.postDelayed(r, 0);
+//                        if (!fcmToken.equals("")) {
+//                            handler.postDelayed(r, 0);
+//                        } else {
+//                            handler.removeCallbacks(r);
+//                        }
                     }
                 });
     }
